46c12b74282816668261f14f3e79a9cf5fb7410c
Mahendra Bagul <bagulm123@gmail.com>
2021-05-15 14:04:27 +0530

    added logger

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@
 module.exports = {
   getEmployeeDetails(client, employeeId) {
     client.getDetails({id: employeeId}, function (err, response) {
+      if (err) {
+        console.log(err)
+      }
       console.log('Employee Details for Employee Id:', employeeId, '\n',
           response);
     });
